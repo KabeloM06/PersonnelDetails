5c860062781f0147b934cca5c3b8049342319a17
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-06-14 18:23:06 +0200

    Completed header

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'PersonnelDetails'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('PersonnelDetails');
+    expect(app.title).toEqual('Personnel Details');
   });
 
   it('should render title', () => {
